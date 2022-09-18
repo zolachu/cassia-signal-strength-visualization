d8988e17ab05acf1e15acc43a80a895c26a40f72
Zola <zolboo@alumni.stanford.edu>
2022-09-17 17:10:25 -0700

    styled record button

--- src/components/UI/RecordButton.js
+++ src/components/UI/RecordButton.js
@@ -1,5 +1,21 @@
 import React, { useState } from "react";
 import styles from "./RecordButton.module.css";
+import StopIcon from "@mui/icons-material/Stop";
+import RadioButtonCheckedIcon from "@mui/icons-material/RadioButtonChecked";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const { palette } = createTheme();
+const { augmentColor } = palette;
+const createColor = (mainColor) => augmentColor({ color: { main: mainColor } });
+const theme = createTheme({
+  palette: {
+    anger: createColor("#EB1D36"),
+    apple: createColor("#5DBA40"),
+    steelBlue: createColor("#5C76B7"),
+    violet: createColor("#BC00A3"),
+  },
+});
 
 const ToggleButton = (props) => {
   const [isToggled, setIsToggled] = useState(true);
@@ -10,20 +26,30 @@ const ToggleButton = (props) => {
     });
     props.onClick();
   }
-  const buttonText = !isToggled ? "STOP" : "RECORD";
+  const buttonText = !isToggled ? "STOP  " : "RECORD";
 
-  const buttonClass = `${styles.button} ${
-    styles[isToggled ? "record" : "stop"]
-  } `;
+  const color = !isToggled ? "anger" : "apple";
 
   return (
-    <button
-      onClick={clickHandler}
-      disabled={props.disable}
-      className={buttonClass}
-    >
-      {buttonText}
-    </button>
+    <ThemeProvider theme={theme}>
+      <Button
+        onClick={clickHandler}
+        disabled={props.disable}
+        color={color}
+        variant="contained"
+        // size="small"
+        style={{ width: "15%" }}
+      >
+        <span className={styles.buttonText}>
+          {buttonText}
+          {!isToggled ? (
+            <StopIcon size="small" />
+          ) : (
+            <RadioButtonCheckedIcon size="small" />
+          )}
+        </span>
+      </Button>
+    </ThemeProvider>
   );
 };
 
