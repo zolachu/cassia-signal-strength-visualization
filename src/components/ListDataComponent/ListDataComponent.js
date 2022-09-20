52af5c897d1ddf23e56edb6a57758eab8dc0151c
Zola <zolboo@alumni.stanford.edu>
2022-09-19 20:37:54 -0700

    list data no record found 28line

--- src/components/ListDataComponent/ListDataComponent.js
+++ src/components/ListDataComponent/ListDataComponent.js
@@ -24,26 +24,30 @@ const DataPoints = (props) => {
   return (
     <div className={styles.container}>
       <h4>Recorded Data Stats</h4>
-      <ul>
-        {data
-          .slice(0)
-          .reverse()
-          .map((item, index) => {
-            console.log(item.length);
-            return item.length > 0 ? (
-              <SingleDataComponent
-                isTimerRunning={props.isTimerRunning}
-                data={item}
-                key={Math.random()}
-                id={index}
-                onDelete={deleteItemHandler}
-                onShow={(data) => props.onShow(data)}
-              ></SingleDataComponent>
-            ) : (
-              <></>
-            );
-          })}
-      </ul>
+      {data[0].length === 0 ? (
+        <h5 style={{ color: "rgb(0,0,0)" }}> No Data Records Available</h5>
+      ) : (
+        <ul>
+          {data
+            .slice(0)
+            .reverse()
+            .map((item, index) => {
+              console.log(item.length);
+              return item.length > 0 ? (
+                <SingleDataComponent
+                  isTimerRunning={props.isTimerRunning}
+                  data={item}
+                  key={Math.random()}
+                  id={index}
+                  onDelete={deleteItemHandler}
+                  onShow={(data) => props.onShow(data)}
+                ></SingleDataComponent>
+              ) : (
+                <></>
+              );
+            })}
+        </ul>
+      )}
     </div>
   );
 };
